4dbd72736adee0fd344d406963968d48b9feafde
Nic Donaldson <nic.donaldson.0@gmail.com>
2018-01-14 23:48:34 +1100

    start adding shared library support to kaleidoscopejit

--- include/KaleidoscopeJIT.h
+++ include/KaleidoscopeJIT.h
@@ -33,104 +33,129 @@
 #include <string>
 #include <vector>
 #include <list>
+#include <iostream>
+#include <map>
+
+#include <dlfcn.h>
 
 namespace llvm {
 namespace orc {
 
 class KaleidoscopeJIT {
-private:
-  std::unique_ptr<TargetMachine> TM;
-  const DataLayout DL;
-  RTDyldObjectLinkingLayer ObjectLayer;
-  IRCompileLayer<decltype(ObjectLayer), SimpleCompiler> CompileLayer;
-  std::list<decltype(CompileLayer)::ModuleHandleT> Handles;
+  private:
+    std::unique_ptr<TargetMachine> TM;
+    const DataLayout DL;
+    RTDyldObjectLinkingLayer ObjectLayer;
+    IRCompileLayer<decltype(ObjectLayer), SimpleCompiler> CompileLayer;
+    std::list<decltype(CompileLayer)::ModuleHandleT> Handles;
+    std::map<std::string,uint64_t> GlobalMap;
 
-public:
-  using ModuleHandle = decltype(CompileLayer)::ModuleHandleT;
+  public:
+    using ModuleHandle = decltype(CompileLayer)::ModuleHandleT;
 
-  KaleidoscopeJIT()
-      : TM(EngineBuilder().selectTarget()), DL(TM->createDataLayout()),
-        ObjectLayer([]() { return std::make_shared<SectionMemoryManager>(); }),
-        CompileLayer(ObjectLayer, SimpleCompiler(*TM)) {
-    llvm::sys::DynamicLibrary::LoadLibraryPermanently(nullptr);
-  }
-
-  TargetMachine &getTargetMachine() { return *TM; }
-
-  ModuleHandle addModule(std::unique_ptr<Module> M) {
-    // Build our symbol resolver:
-    // Lambda 1: Look back into the JIT itself to find symbols that are part of
-    //           the same "logical dylib".
-    // Lambda 2: Search for external symbols in the host process.
-    auto Resolver = createLambdaResolver(
-        [&](const std::string &Name) {
-          if (auto Sym = CompileLayer.findSymbol(Name, false))
-            return Sym;
-          return JITSymbol(nullptr);
-        },
-        [](const std::string &Name) {
-          if (auto SymAddr =
-                RTDyldMemoryManager::getSymbolAddressInProcess(Name))
-            return JITSymbol(SymAddr, JITSymbolFlags::Exported);
-          return JITSymbol(nullptr);
-        });
-
-    // Add the set to the JIT with the resolver we created above and a newly
-    // created SectionMemoryManager.
-    auto handle = cantFail(CompileLayer.addModule(std::move(M),
-                                                  std::move(Resolver)));
-    Handles.push_back(handle);
-    return handle;
-  }
-
-  JITSymbol findSymbol(const std::string Name) {
-    std::string MangledName;
-    raw_string_ostream MangledNameStream(MangledName);
-    Mangler::getNameWithPrefix(MangledNameStream, Name, DL);
-
-    for (auto it = Handles.rbegin(); it != Handles.rend(); ++it) {
-        auto sym = CompileLayer.findSymbolIn(*it, MangledNameStream.str(), true);
-        if (sym) {
-            return sym;
-        }
+    KaleidoscopeJIT()
+            : TM(EngineBuilder().selectTarget()), DL(TM->createDataLayout()),
+              ObjectLayer([]() { return std::make_shared<SectionMemoryManager>(); }),
+              CompileLayer(ObjectLayer, SimpleCompiler(*TM)) {
+        llvm::sys::DynamicLibrary::LoadLibraryPermanently(nullptr);
     }
 
-    return CompileLayer.findSymbol(MangledNameStream.str(), true);
-  }
+    void * openLibrary(const std::string& name) {
+        dlerror();
+        auto lib_ptr = dlopen(name.c_str(), RTLD_NOW);
+        if (lib_ptr == nullptr) {
+            std::cout << "Failed to open library with dlopen(): " << dlerror() << std::endl;
+            return nullptr;
+        }
 
-  void removeModule(ModuleHandle H) {
-    cantFail(CompileLayer.removeModule(H));
-  }
+        return lib_ptr;
+    }
 
-  void *getPointerToFunction(Function *F) {
-      auto sym = findSymbol(F->getName().str());
-      if (sym) {
-          auto expected_addr = sym.getAddress();
-          if (expected_addr) {
-              return (void*)expected_addr.get();
-          }
-      }
-     return nullptr;
-  }
+    bool bindSymbol(void * library_ptr, const std::string& sym) {
+        auto ptr(dlsym(library_ptr, sym.c_str()));
+        if (ptr) {
+            GlobalMap.insert(std::pair<std::string,uint64_t>(sym, (uint64_t)ptr));
+            return true;
+        }
+        return false;
+    }
 
-  llvm::GenericValue runFunction(llvm::Function *F,
-                                 llvm::ArrayRef<llvm::GenericValue> ArgValues) {
-      auto func_type = F->getFunctionType();
-      auto return_type = F->getReturnType();
+    TargetMachine &getTargetMachine() { return *TM; }
 
-      assert (func_type->getNumParams() == 0);
-      assert (ArgValues.size() == 0);
-      assert (return_type->getTypeID() == Type::VoidTyID);
+    ModuleHandle addModule(std::unique_ptr<Module> M) {
+        // Build our symbol resolver:
+        // Lambda 1: Look back into the JIT itself to find symbols that are part of
+        //           the same "logical dylib".
+        // Lambda 2: Search for external symbols in the host process.
+        auto Resolver = createLambdaResolver(
+            [&](const std::string &Name) {
+                if (auto Sym = CompileLayer.findSymbol(Name, false))
+                    return Sym;
+                return JITSymbol(nullptr);
+            },
+            [](const std::string &Name) {
+                if (auto SymAddr =
+                    RTDyldMemoryManager::getSymbolAddressInProcess(Name))
+                    return JITSymbol(SymAddr, JITSymbolFlags::Exported);
+                return JITSymbol(nullptr);
+            });
 
-      // No arguments, void function
-      void (*f)() =  (void (*)())getPointerToFunction(F);
+        // Add the set to the JIT with the resolver we created above and a newly
+        // created SectionMemoryManager.
+        auto handle = cantFail(CompileLayer.addModule(std::move(M),
+                                                      std::move(Resolver)));
+        Handles.push_back(handle);
+        return handle;
+    }
 
-      f();
+    JITSymbol findSymbol(const std::string Name) {
+        std::string MangledName;
+        raw_string_ostream MangledNameStream(MangledName);
+        Mangler::getNameWithPrefix(MangledNameStream, Name, DL);
 
-      llvm::GenericValue rv;
-      rv.DoubleVal = 1.0;
-      return rv;
-  }
+        for (auto it = Handles.rbegin(); it != Handles.rend(); ++it) {
+            auto sym = CompileLayer.findSymbolIn(*it, MangledNameStream.str(), true);
+            if (sym) {
+                return sym;
+            }
+        }
+
+        return CompileLayer.findSymbol(MangledNameStream.str(), true);
+    }
+
+    void removeModule(ModuleHandle H) {
+        cantFail(CompileLayer.removeModule(H));
+    }
+
+    void *getPointerToFunction(Function *F) {
+        auto sym = findSymbol(F->getName().str());
+        if (sym) {
+            auto expected_addr = sym.getAddress();
+            if (expected_addr) {
+                return (void*)expected_addr.get();
+            }
+        }
+        return nullptr;
+    }
+
+    llvm::GenericValue runFunction(llvm::Function *F,
+                                   llvm::ArrayRef<llvm::GenericValue> ArgValues) {
+        auto func_type = F->getFunctionType();
+        auto return_type = F->getReturnType();
+
+        assert (func_type->getNumParams() == 0);
+        assert (ArgValues.size() == 0);
+        assert (return_type->getTypeID() == Type::VoidTyID);
+
+        // No arguments, void function
+        void (*f)() =  (void (*)())getPointerToFunction(F);
+
+        f();
+
+        llvm::GenericValue rv;
+        rv.DoubleVal = 1.0;
+        return rv;
+    }
 };
 
 } // end namespace orc
